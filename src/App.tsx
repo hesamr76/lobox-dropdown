f2f1802d7ca00caae1a9c1dabc714bbe253266c3
Hesam Rajaei <hesamr76@gmail.com>
2025-05-15 02:09:31 -0700

    Add SASS support and create App.module.scss for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import styles from "./App.module.scss";
+
 function App() {
-  return <h1>Multi Select Dropdown</h1>;
+  return (
+    <div className={styles.container}>
+      <h1>Multi Select Dropdown</h1>
+    </div>
+  );
 }
 
 export default App;
